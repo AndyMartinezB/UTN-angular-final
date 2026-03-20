f9f699a0032adea9e6c9c7bb3bb57dc75ce8bfea
Andy <andres.bologna@mi.unc.edu.ar>
2026-03-20 14:10:43 -0300

    feat: chats view y app

--- src/app/components/chat-list/chat-list.ts
+++ src/app/components/chat-list/chat-list.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ChatService } from '../../services/chat.service';
 
 @Component({
   selector: 'app-chat-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './chat-list.html',
   styleUrl: './chat-list.css',
 })
-export class ChatList {}
+export class ChatList {
+  chatService = inject(ChatService);
+
+  chats = this.chatService.chats;
+}
--- src/app/views/chats/chats.ts
+++ src/app/views/chats/chats.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ChatList } from '../../components/chat-list/chat-list';
 
 @Component({
   selector: 'app-chats',
   standalone: true,
-  imports: [],
+  imports: [ChatList],
   templateUrl: './chats.html',
   styleUrl: './chats.css',
 })
