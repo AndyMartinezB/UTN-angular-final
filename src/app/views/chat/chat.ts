569ae12cc00fd28b41f825baf3952d6f63ad7e83
Andy <andres.bologna@mi.unc.edu.ar>
2026-03-20 14:55:30 -0300

    fix: navegacion entre chats

--- src/app/views/chat/chat.ts
+++ src/app/views/chat/chat.ts
@@ -2,6 +2,8 @@ import { Component, inject, computed } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ReactiveFormsModule, FormControl } from '@angular/forms';
 import { ChatService } from '../../services/chat.service';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
@@ -14,9 +16,11 @@ export class Chat {
   private route = inject(ActivatedRoute);
   private chatService = inject(ChatService);
 
-  chatId = Number(this.route.snapshot.paramMap.get('id')!);
+  chatId = toSignal(this.route.paramMap.pipe(map((params) => Number(params.get('id')))), {
+    initialValue: 0,
+  });
 
-  chat = computed(() => this.chatService.getChat(this.chatId));
+  chat = computed(() => this.chatService.getChat(this.chatId()));
 
   messageControl = new FormControl<string>('');
 
@@ -25,7 +29,7 @@ export class Chat {
 
     if (!text) return;
 
-    this.chatService.sendMessage(this.chatId, text);
+    this.chatService.sendMessage(this.chatId(), text);
 
     this.messageControl.setValue('');
   }
