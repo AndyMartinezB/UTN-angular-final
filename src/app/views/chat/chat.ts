116d599b62f211099e350980ba31c071baa032f9
Andy <andres.bologna@mi.unc.edu.ar>
2026-03-20 14:14:36 -0300

    ReactiveForm en chat

--- src/app/views/chat/chat.ts
+++ src/app/views/chat/chat.ts
@@ -1,11 +1,12 @@
 import { Component, inject, computed } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
 import { ChatService } from '../../services/chat.service';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './chat.html',
   styleUrl: './chat.css',
 })
@@ -16,4 +17,16 @@ export class Chat {
   chatId = Number(this.route.snapshot.paramMap.get('id'));
 
   chat = computed(() => this.chatService.getChat(this.chatId));
+
+  messageControl = new FormControl('');
+
+  sendMessage() {
+    const text = this.messageControl.value;
+
+    if (!text) return;
+
+    this.chatService.sendMessage(this.chatId, text);
+
+    this.messageControl.setValue('');
+  }
 }
